1ade8f2b764f19dc8fe8ebf38589331656fa90be
Jose <113186145+jose-yanes@users.noreply.github.com>
2025-01-13 15:45:21 -0300

    Player finished

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 )
 
@@ -14,9 +19,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0, 0, 0))
-        player.draw(screen)
+
+        for object in updatable:
+            object.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
+
+        #Limit FPS to 60
         dt = clock.tick(60) / 1000
 
 
